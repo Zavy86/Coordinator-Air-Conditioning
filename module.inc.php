2545f56151b79442612200bf8834a9a765dfcd33
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-04-07 16:57:26 +0200

    Module informations

---
+++ module.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Air Conditioning
+ *
+ * @package Coordinator\Modules\Air-Conditioning
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.zavynet.org
+ */
+ $module_name="air-conditioning";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Air-Conditioning/master/VERSION.txt";
+?>
